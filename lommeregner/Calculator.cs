2a2bca73bcd038a27e76651d089ecb7f35a7d029
Jim Haaland Damgaard <jimdenseje@gmail.com>
2022-08-01 12:08:57 +0200

    init

---
+++ lommeregner/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lommeregner
+{
+    public static class Calculator
+    {
+        public static string Calc(this string source) 
+        {
+
+            return source;
+        }
+    }
+}
--- lommeregner/Form1.Designer.cs
+++ lommeregner/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace lommeregner
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Display = new System.Windows.Forms.TextBox();
+            this.History = new System.Windows.Forms.RichTextBox();
+            this.SubtractBtn = new System.Windows.Forms.Button();
+            this.MultiplyBtn = new System.Windows.Forms.Button();
+            this.DivideBtn = new System.Windows.Forms.Button();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.EqualBtn = new System.Windows.Forms.Button();
+            this.Num_8 = new System.Windows.Forms.Button();
+            this.Num_7 = new System.Windows.Forms.Button();
+            this.Num_9 = new System.Windows.Forms.Button();
+            this.Num_4 = new System.Windows.Forms.Button();
+            this.Num_5 = new System.Windows.Forms.Button();
+            this.Num_6 = new System.Windows.Forms.Button();
+            this.Num_1 = new System.Windows.Forms.Button();
+            this.Num_2 = new System.Windows.Forms.Button();
+            this.Num_3 = new System.Windows.Forms.Button();
+            this.Num_0 = new System.Windows.Forms.Button();
+            this.Num_Decimal = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.BackColor = System.Drawing.Color.White;
+            this.Display.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Display.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Display.Location = new System.Drawing.Point(0, 0);
+            this.Display.Name = "Display";
+            this.Display.ReadOnly = true;
+            this.Display.Size = new System.Drawing.Size(607, 31);
+            this.Display.TabIndex = 0;
+            // 
+            // History
+            // 
+            this.History.BackColor = System.Drawing.Color.White;
+            this.History.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.History.Location = new System.Drawing.Point(12, 37);
+            this.History.Name = "History";
+            this.History.ReadOnly = true;
+            this.History.Size = new System.Drawing.Size(226, 289);
+            this.History.TabIndex = 1;
+            this.History.Text = "";
+            // 
+            // SubtractBtn
+            // 
+            this.SubtractBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SubtractBtn.Location = new System.Drawing.Point(487, 214);
+            this.SubtractBtn.Name = "SubtractBtn";
+            this.SubtractBtn.Size = new System.Drawing.Size(112, 53);
+            this.SubtractBtn.TabIndex = 2;
+            this.SubtractBtn.Text = "-";
+            this.SubtractBtn.UseVisualStyleBackColor = true;
+            // 
+            // MultiplyBtn
+            // 
+            this.MultiplyBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MultiplyBtn.Location = new System.Drawing.Point(487, 155);
+            this.MultiplyBtn.Name = "MultiplyBtn";
+            this.MultiplyBtn.Size = new System.Drawing.Size(112, 53);
+            this.MultiplyBtn.TabIndex = 3;
+            this.MultiplyBtn.Text = "X";
+            this.MultiplyBtn.UseVisualStyleBackColor = true;
+            // 
+            // DivideBtn
+            // 
+            this.DivideBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DivideBtn.Location = new System.Drawing.Point(487, 96);
+            this.DivideBtn.Name = "DivideBtn";
+            this.DivideBtn.Size = new System.Drawing.Size(112, 53);
+            this.DivideBtn.TabIndex = 4;
+            this.DivideBtn.Text = "/";
+            this.DivideBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddBtn.Location = new System.Drawing.Point(487, 273);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(112, 53);
+            this.AddBtn.TabIndex = 5;
+            this.AddBtn.Text = "+";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            // 
+            // EqualBtn
+            // 
+            this.EqualBtn.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.EqualBtn.Location = new System.Drawing.Point(244, 273);
+            this.EqualBtn.Name = "EqualBtn";
+            this.EqualBtn.Size = new System.Drawing.Size(237, 53);
+            this.EqualBtn.TabIndex = 6;
+            this.EqualBtn.Text = "=";
+            this.EqualBtn.UseVisualStyleBackColor = true;
+            // 
+            // Num_8
+            // 
+            this.Num_8.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_8.Location = new System.Drawing.Point(325, 37);
+            this.Num_8.Name = "Num_8";
+            this.Num_8.Size = new System.Drawing.Size(75, 53);
+            this.Num_8.TabIndex = 7;
+            this.Num_8.Text = "8";
+            this.Num_8.UseVisualStyleBackColor = true;
+            // 
+            // Num_7
+            // 
+            this.Num_7.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_7.Location = new System.Drawing.Point(244, 37);
+            this.Num_7.Name = "Num_7";
+            this.Num_7.Size = new System.Drawing.Size(75, 53);
+            this.Num_7.TabIndex = 8;
+            this.Num_7.Text = "7";
+            this.Num_7.UseVisualStyleBackColor = true;
+            // 
+            // Num_9
+            // 
+            this.Num_9.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_9.Location = new System.Drawing.Point(406, 37);
+            this.Num_9.Name = "Num_9";
+            this.Num_9.Size = new System.Drawing.Size(75, 53);
+            this.Num_9.TabIndex = 9;
+            this.Num_9.Text = "9";
+            this.Num_9.UseVisualStyleBackColor = true;
+            // 
+            // Num_4
+            // 
+            this.Num_4.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_4.Location = new System.Drawing.Point(244, 96);
+            this.Num_4.Name = "Num_4";
+            this.Num_4.Size = new System.Drawing.Size(75, 53);
+            this.Num_4.TabIndex = 10;
+            this.Num_4.Text = "4";
+            this.Num_4.UseVisualStyleBackColor = true;
+            // 
+            // Num_5
+            // 
+            this.Num_5.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_5.Location = new System.Drawing.Point(325, 96);
+            this.Num_5.Name = "Num_5";
+            this.Num_5.Size = new System.Drawing.Size(75, 53);
+            this.Num_5.TabIndex = 11;
+            this.Num_5.Text = "5";
+            this.Num_5.UseVisualStyleBackColor = true;
+            // 
+            // Num_6
+            // 
+            this.Num_6.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_6.Location = new System.Drawing.Point(406, 96);
+            this.Num_6.Name = "Num_6";
+            this.Num_6.Size = new System.Drawing.Size(75, 53);
+            this.Num_6.TabIndex = 12;
+            this.Num_6.Text = "6";
+            this.Num_6.UseVisualStyleBackColor = true;
+            // 
+            // Num_1
+            // 
+            this.Num_1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_1.Location = new System.Drawing.Point(244, 155);
+            this.Num_1.Name = "Num_1";
+            this.Num_1.Size = new System.Drawing.Size(75, 53);
+            this.Num_1.TabIndex = 13;
+            this.Num_1.Text = "1";
+            this.Num_1.UseVisualStyleBackColor = true;
+            // 
+            // Num_2
+            // 
+            this.Num_2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_2.Location = new System.Drawing.Point(325, 155);
+            this.Num_2.Name = "Num_2";
+            this.Num_2.Size = new System.Drawing.Size(75, 53);
+            this.Num_2.TabIndex = 14;
+            this.Num_2.Text = "2";
+            this.Num_2.UseVisualStyleBackColor = true;
+            // 
+            // Num_3
+            // 
+            this.Num_3.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_3.Location = new System.Drawing.Point(406, 155);
+            this.Num_3.Name = "Num_3";
+            this.Num_3.Size = new System.Drawing.Size(75, 53);
+            this.Num_3.TabIndex = 15;
+            this.Num_3.Text = "3";
+            this.Num_3.UseVisualStyleBackColor = true;
+            // 
+            // Num_0
+            // 
+            this.Num_0.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_0.Location = new System.Drawing.Point(244, 214);
+            this.Num_0.Name = "Num_0";
+            this.Num_0.Size = new System.Drawing.Size(156, 53);
+            this.Num_0.TabIndex = 17;
+            this.Num_0.Text = "0";
+            this.Num_0.UseVisualStyleBackColor = true;
+            // 
+            // Num_Decimal
+            // 
+            this.Num_Decimal.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Num_Decimal.Location = new System.Drawing.Point(406, 214);
+            this.Num_Decimal.Name = "Num_Decimal";
+            this.Num_Decimal.Size = new System.Drawing.Size(75, 53);
+            this.Num_Decimal.TabIndex = 18;
+            this.Num_Decimal.Text = ".";
+            this.Num_Decimal.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DeleteBtn.Location = new System.Drawing.Point(487, 37);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(112, 53);
+            this.DeleteBtn.TabIndex = 19;
+            this.DeleteBtn.Text = "«";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(607, 332);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.Num_Decimal);
+            this.Controls.Add(this.Num_0);
+            this.Controls.Add(this.Num_3);
+            this.Controls.Add(this.Num_2);
+            this.Controls.Add(this.Num_1);
+            this.Controls.Add(this.Num_6);
+            this.Controls.Add(this.Num_5);
+            this.Controls.Add(this.Num_4);
+            this.Controls.Add(this.Num_9);
+            this.Controls.Add(this.Num_7);
+            this.Controls.Add(this.Num_8);
+            this.Controls.Add(this.EqualBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.DivideBtn);
+            this.Controls.Add(this.MultiplyBtn);
+            this.Controls.Add(this.SubtractBtn);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.Display);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Lommeregner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox Display;
+        private RichTextBox History;
+        private Button SubtractBtn;
+        private Button MultiplyBtn;
+        private Button DivideBtn;
+        private Button AddBtn;
+        private Button EqualBtn;
+        private Button Num_8;
+        private Button Num_7;
+        private Button Num_9;
+        private Button Num_4;
+        private Button Num_5;
+        private Button Num_6;
+        private Button Num_1;
+        private Button Num_2;
+        private Button Num_3;
+        private Button Num_0;
+        private Button Num_Decimal;
+        private Button DeleteBtn;
     }
 }
